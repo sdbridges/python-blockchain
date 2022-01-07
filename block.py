c41457a388230a752d18c0909fc482558c7c7cb7
sdbridges <sdbridges88@gmail.com>
2022-01-07 17:42:47 -0500

    Refactor using static decorators, update add_block

--- block.py
+++ block.py
@@ -1,23 +1,6 @@
 # Mining = spend CPU power
 import time
 
-
-def mine_block(last_block, data):
-    """
-    Mine a block based on the given last_block and data.
-    """
-    timestamp = time.time_ns()
-    last_hash = last_block.hash
-    hash = f'{timestamp}-{last_hash}'
-
-    return Block(timestamp, last_hash, hash, data)
-
-def genesis():
-    """
-    Generate a genesis block. (hardcoded)
-    """
-    return Block(1, 'genesis_last_hash', 'genesis_hash', [])
-
 class Block:
     """
     Block: unit of storage.
@@ -38,9 +21,27 @@ class Block:
             f'data: {self.data})' #transactions, etc.
         )
 
+    @staticmethod
+    def mine_block(last_block, data):
+        """
+        Mine a block based on the given last_block and data.
+        """
+        timestamp = time.time_ns()
+        last_hash = last_block.hash
+        hash = f'{timestamp}-{last_hash}'
+
+        return Block(timestamp, last_hash, hash, data)
+
+    @staticmethod
+    def genesis():
+        """
+        Generate a genesis block. (hardcoded)
+        """
+        return Block(1, 'genesis_last_hash', 'genesis_hash', [])
+
 def main():
-    genesis_block = genesis()
-    block = mine_block(genesis_block, 'foo')
+    genesis_block = Block.genesis()
+    block = Block.mine_block(genesis_block, 'foo')
     print(block)
 
 if __name__ == '__main__':
