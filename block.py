cb5fa466b391c87dde94a6fe1dae7b41df42015a
sdbridges <sdbridges88@gmail.com>
2022-01-08 12:38:50 -0500

    Finish hashing stringified(64 hex)  algorithm(sha256) and refactor using lambda function (mapping hashed arg functions)

--- block.py
+++ block.py
@@ -1,6 +1,8 @@
 # Mining = spend CPU power
 import time
 
+from crypto_hash import crypto_hash
+
 class Block:
     """
     Block: unit of storage.
@@ -28,7 +30,7 @@ class Block:
         """
         timestamp = time.time_ns()
         last_hash = last_block.hash
-        hash = f'{timestamp}-{last_hash}'
+        hash = crypto_hash(timestamp, last_hash, data)
 
         return Block(timestamp, last_hash, hash, data)
 
--- crypto_hash.py
+++ crypto_hash.py
@@ -1,16 +1,24 @@
 import hashlib
 import json
 
-def crypto_hash(data):
-    """
-    Return a SHA-256 hash of the given data.
-    """
-    stringified_data = json.dumps(data)
+# def stringify(data):
+#     return json.dumps(data)
 
-    return hashlib.sha256(stringified_data.encode('utf-8')).hexdigest()
+def crypto_hash(*args):
+    """
+    Return a SHA-256 hash of the given arguments.
+    """
+    stringified_args = sorted(map(lambda data: json.dumps(data), args))
+    print(f'stringified_args: {stringified_args}')
+
+    joined_data = ''.join(stringified_args)
+
+    print (f'joined_data: {joined_data}')
+
+    return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
 def main():
-    print(f"crypto_hash(1): {crypto_hash(1)}")
+    print(f"crypto_hash('one', 2, [3]): {crypto_hash('one', 2, [3])}")
 
 if __name__ == '__main__':
     main()
